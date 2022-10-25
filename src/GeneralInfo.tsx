7c08fd2a0603a9cf17b0664442d81a98388175b4
tornike jandieri <tornikejandieri7@gmail.com>
2022-10-25 19:19:38 +0400

    first input js

--- src/GeneralInfo.tsx
+++ src/GeneralInfo.tsx
@@ -1,6 +1,31 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { nanoid } from "nanoid";
 
 const GeneralInfo: React.FC = () => {
+  const [firstName, setFirstName] = useState<string>('')
+  const [lastName, setLastName] = useState<string>('')
+  const [placeOfResidence, setPlaceOfResidence] = useState<string>('')
+  const [sumarry, setSumarry] = useState<string>('')
+  const [generalInfo, setGeneralInfo] = useState<any>([])
+
+  const navigate = useNavigate()
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault()
+    if(firstName && lastName && placeOfResidence && sumarry){
+      const info = {id: nanoid(), firstName, lastName, placeOfResidence, sumarry }
+      setGeneralInfo((generalInfo: any) => {
+       return [...generalInfo, info]
+      })
+      setFirstName('')
+      setLastName('')
+      setPlaceOfResidence('')
+      setSumarry('')
+    }
+    console.log(firstName, lastName, placeOfResidence, sumarry)
+    navigate('/workexperience')
+  }
   return ( 
     <div className="general-info">
       <div>
@@ -9,29 +34,29 @@ const GeneralInfo: React.FC = () => {
       
       <div className="container">
         <article>
-          <form className="form-control">
+          <form className="form-control" onSubmit={handleSubmit}>
             <div>
               <label htmlFor="firstName">Name </label>
-              <input type="text" id="name" />
+              <input type="text" id="name" value={firstName} onChange={(e: any) => setFirstName(e.target.value)} />
             </div>
             <div>
               <label htmlFor="lastName">Last Name </label>
-              <input type="text" id="lastname" />
+              <input type="text" id="lastname" value={lastName} onChange={(e: any) => setLastName(e.target.value)} />
             </div>
             <div>
               <label htmlFor="location">Place of residence </label>
-              <input type="text" id="location" />
+              <input type="text" id="location" value={placeOfResidence} onChange={(e: any) => setPlaceOfResidence(e.target.value)} />
             </div>
             <div>
               <label htmlFor="sumarry">Sumarry about yourself </label>
-              <textarea  id="summary" placeholder="Write small summary about your skills and profession for overview purposes" />
+              <textarea  
+              id="summary"
+              placeholder="Write small summary about your skills and profession for overview purposes"
+              value={sumarry}
+              onChange={(e: any) => setSumarry(e.target.value)} />
             </div>
             <button type="submit">Save</button>
           </form>
-          
-
-          {/* put programming navigation link inside submit function for later */}
-          <Link to={'/workexperience'}>Next Step</Link>
         </article>
       </div>
     </div>
